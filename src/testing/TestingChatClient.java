bf3d543de39fa884cf08e564ed0c7ff688badb39
Vedad Zuga <vedadz@gmail.com>
2016-05-16 18:35:04 +0200

    add fully featured testing client

--- src/testing/TestingChatClient.java
+++ src/testing/TestingChatClient.java
@@ -1,18 +1,21 @@
 package testing;
 
-import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
 import protocol.JoinRequest;
+import protocol.LeaveRequest;
+import protocol.MembersRequest;
+import protocol.MessageRequest;
 import protocol.Request;
+import protocol.RoomsRequest;
 
 public class TestingChatClient extends Thread {
-	
+
 	private Socket s;
-	
+
 	public TestingChatClient(String host, int port) {
 		try {
 			s = new Socket(host, port);
@@ -20,7 +23,7 @@ public class TestingChatClient extends Thread {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void run() {
 		new MessagePrinter(s).start();
 		Scanner kb = new Scanner(System.in);
@@ -29,30 +32,38 @@ public class TestingChatClient extends Thread {
 			out.flush();
 			while (!s.isClosed()) {
 				String msg = kb.nextLine();
-				if(msg.startsWith("/join")){
-					System.out.println(msg);
-					Request r = new JoinRequest("tempname", "temp");
-					System.out.println("Made request");
+				Request r = null;
+				if (msg.startsWith("/join")) {
+					r = new JoinRequest("tempUser3", "tempRoom");
+				} else if (msg.startsWith("/rooms")) {
+					r = new RoomsRequest();
+				} else if (msg.startsWith("/members")) {
+					r = new MembersRequest("tempRoom");
+				} else if (msg.startsWith("/leave")) {
+					r = new LeaveRequest();
+				} else {
+					r = new MessageRequest(msg);
+				}
+				if (r != null) {
 					out.writeObject(r);
-					System.out.println("Wrote request");
 					out.flush();
-					System.out.println("Sent /join request");
 				}
 			}
-		} catch (IOException e) {}
+		} catch (IOException e) {
+		}
 		kb.close();
 	}
 
 	public static void main(String[] args) {
 		// synopsis: java ChatClient machine port
-//		if (args.length != 2) {
-//			System.out.println("usage: java ChatClient machine port");
-//			System.exit(1);
-//		}
-		
+		// if (args.length != 2) {
+		// System.out.println("usage: java ChatClient machine port");
+		// System.exit(1);
+		// }
+
 		String machine = "localhost";
 		int port = 30000;
-		
+
 		new TestingChatClient(machine, port).run();
 	}
 
